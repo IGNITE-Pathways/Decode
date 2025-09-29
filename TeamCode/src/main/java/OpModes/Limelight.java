42a2c61626fa7486669564df2933099ef27b6f20
itzSamar <samarreal15@gmail.com>
2025-09-28 17:56:34 -0700

    Did limelight updated dependencies also updates flywheel

---
+++ TeamCode/src/main/java/OpModes/Limelight.java
@@ -0,0 +1,39 @@
+package OpModes;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Limelight", group="Linear OpMode")
+public class Limelight extends OpMode {
+    private Limelight3A limelight;
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        if (limelight != null) {
+            limelight.setPollRateHz(100);
+            limelight.start();
+        }
+    }
+
+    @Override
+    public void loop() {
+        if (limelight == null || !limelight.isConnected()) {
+            telemetry.addLine("Limelight not connected");
+        } else {
+            LLResult result = limelight.getLatestResult();
+
+            if (result != null && result.isValid()) {
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("ta", result.getTa());
+                telemetry.addData("BotPose", result.getBotpose().toString());
+            } else {
+                telemetry.addLine("No AprilTag detected");
+            }
+        }
+        telemetry.update();
+    }
+}
--- TeamCode/src/main/java/ProgrammingBoard/ProgrammingBoard.java
+++ TeamCode/src/main/java/ProgrammingBoard/ProgrammingBoard.java
@@ -7,12 +7,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class ProgrammingBoard {
 
     ;
-    public DcMotor leftFrontDrive = null;
+    /*public DcMotor leftFrontDrive = null;
     public DcMotor leftBackDrive = null;
     public DcMotor rightFrontDrive = null;
-    public DcMotor rightBackDrive = null;
+    public DcMotor rightBackDrive = null;*/
+    public DcMotor flyWheelMotor = null;
+
 
     public void initializeComponents(HardwareMap hwMap) {
+        /* 
         leftFrontDrive = hwMap.get(DcMotor.class, "leftfrontmotor");
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
@@ -24,12 +27,16 @@ public class ProgrammingBoard {
 
         rightBackDrive = hwMap.get(DcMotor.class, "rightbackmotor");
         rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
+        */
+        flyWheelMotor = hwMap.get(DcMotor.class, "flywheelmotor");
+//        flyWheelMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        /* 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
+        */
+        flyWheelMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
 }
